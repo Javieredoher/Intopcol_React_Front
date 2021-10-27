ea94055b5083b6619b7c872fafa71f1187f1412f
DaniOspina <danielaos94@gmail.com>
2021-10-26 22:04:28 -0500

    import of useContext correction

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,5 +1,4 @@
-import React, {useState} from 'react'
-import { useContext } from 'react/cjs/react.development';
+import React, {useState, useContext} from 'react'
 import { AuthContext } from '../auth/AuthContext';
 import { ChatContext } from '../context/chat/ChatContext';
 import {SocketContext} from '../context/SocketContext'
