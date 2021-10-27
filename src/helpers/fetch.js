089c82d6d2396322d576a9df42ea33714f958fb6
Javieredoher <javieredoher.co@gmail.com>
2021-10-26 21:09:28 -0500

    fix

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,4 +1,6 @@
-const baseUrl = process.env.REACT_APP_API_URL;
+//const baseUrl = process.env.REACT_APP_API_URL;
+
+const baseUrl = 'https://chatintopcol.herokuapp.com/api';
 
 //Rename functions to English before send to production
 
